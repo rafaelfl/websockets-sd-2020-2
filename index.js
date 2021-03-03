33924b6bcca0cd5ee587aae0c8dd336daabfae67
Rafael Fernandes Lopes <rafael.fernandes@gmail.com>
2021-03-02 21:32:43 -0300

    Importado o express

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
+const express = require("express");
+
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Servidor up and running na porta ${PORT}!`));
